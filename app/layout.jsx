659567bf29f09f875666ceb6bc641d39b1099a39
homeprotec <homeprotec.es@gmail.com>
2026-02-11 01:42:08 -0600

    Create layout.jsx

---
+++ app/layout.jsx
@@ -0,0 +1,14 @@
+export const metadata = {
+  title: "HOME PROTECT",
+  description: "Sistemas de seguridad y redes",
+}
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="es">
+      <body>
+        {children}
+      </body>
+    </html>
+  )
+}
